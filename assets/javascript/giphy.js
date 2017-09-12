70813e48081e2dffdd1d578ee0905bbbcc0af368
sal.szu <sal.szu@gmail.com>
2017-09-12 16:26:55 -0400

    playing with isotope

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -56,7 +56,18 @@ window.onload = function() {
     createButtons();
     giphyClick();
 
+var elem = document.querySelector('.grid');
+var iso = new Isotope( elem, {
+  // options
+  itemSelector: '.grid-item',
+  layoutMode: 'fitRows'
+});
 
+// element argument can be a selector string
+//   for an individual element
+var iso = new Isotope( '.grid', {
+  // options
+});
 
 
 };
